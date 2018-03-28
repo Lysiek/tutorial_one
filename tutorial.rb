0a285e2ee892fb8757428c9cf44e0e7715544921
Lysiek <lysiekchoun168@gmail.com>
2018-03-28 08:52:20 +0700

    Second commit file

--- tutorial.rb
+++ tutorial.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
+class UsersController < ApplicationController
+	def index
+		
+	end
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new user_params
+		if @user.save
+			flash[:success] = "Register success"
+			redirect_to user_path
+		else
+			flash[:success] = "Register failed"
+			render :new
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit :name, :password, :password_confirmation
+		
+	end
+
+end
